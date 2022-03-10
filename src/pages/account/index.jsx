cb1257715956ee16de808cee564ebc6c1b8f5405
bhaktibuana <bhaktibuana19@gmail.com>
2022-03-10 21:24:30 +0700

    create initial pages

---
+++ src/pages/account/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "../../components/admin/sidebar";
+
+import "./style.scss";
+
+const Account = () => {
+  return (
+    <>
+      <Sidebar pageName="account" />
+    </>
+  );
+};
+
+export default Account;
