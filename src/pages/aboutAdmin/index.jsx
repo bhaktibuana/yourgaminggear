32c72f44d95ee0b0d440ed8011b565230be5e284
bhaktibuana <bhaktibuana19@gmail.com>
2022-03-11 20:42:28 +0700

    modify and style about admin page

--- src/pages/aboutAdmin/index.jsx
+++ src/pages/aboutAdmin/index.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import Navbar from "../../components/admin/navbar";
+import RightSidebar from "../../components/admin/rightSidebar";
 import Sidebar from "../../components/admin/sidebar";
 
 import "./style.scss";
 
-const AboutAdmin = () => {
+const AboutAdmin = (props) => {
   return (
     <>
-      <Sidebar pageName="aboutAdmin" />
+      <div className={props.appState.isExpand ? "about-admin-expand" : "about-admin"}>
+        <RightSidebar pageName="aboutAdmin" appState={props.appState} />
+        <Navbar pageName="About" appState={props.appState} />
+
+        <div className="about-admin-container">
+          <h1>About Page</h1>
+        </div>
+      </div>
+      <Sidebar pageName="aboutAdmin" appState={props.appState} />
     </>
   );
 };
