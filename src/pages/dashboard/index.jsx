95153c8bad04d71a2ebdbc9e6373ce0095073020
bhaktibuana <bhaktibuana19@gmail.com>
2022-03-11 20:41:43 +0700

    modify and style dashboard page

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import Navbar from "../../components/admin/navbar";
+import RightSidebar from "../../components/admin/rightSidebar";
 import Sidebar from "../../components/admin/sidebar";
 
 import "./style.scss";
 
-const Dashboard = () => {
+const Dashboard = (props) => {
   return (
     <>
-      <Sidebar pageName="dashboard" />
+      <div className={props.appState.isExpand ? "dashboard-expand" : "dashboard"}>
+        <RightSidebar pageName="dashboard" appState={props.appState} />
+        <Navbar pageName="Dashboard" appState={props.appState} />
+        
+        <div className="dashboard-container">
+          <h1>Dashboard Page</h1>
+        </div>
+      </div>
+      <Sidebar pageName="dashboard" appState={props.appState} />
     </>
   );
 };
