bd246978bcb9817322efe7e00ac712647e44d98b
bhaktibuana <bhaktibuana19@gmail.com>
2022-03-11 20:42:01 +0700

    modify and style account page

--- src/pages/account/index.jsx
+++ src/pages/account/index.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import Navbar from "../../components/admin/navbar";
 import Sidebar from "../../components/admin/sidebar";
+import RightSidebar from "../../components/admin/rightSidebar";
 
 import "./style.scss";
 
-const Account = () => {
+const Account = (props) => {
   return (
     <>
-      <Sidebar pageName="account" />
+      <div className={props.appState.isExpand ? "account-expand" : "account"}>
+        <RightSidebar pageName="account" appState={props.appState} />
+        <Navbar pageName="Account" appState={props.appState} />
+
+        <div className="account-container">
+          <h1>Account Setting Page</h1>
+        </div>
+      </div>
+      <Sidebar pageName="account" appState={props.appState} />
     </>
   );
 };
