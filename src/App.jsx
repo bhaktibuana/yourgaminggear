5a17aeeff915bb314b4abd63fc762c35a973002a
bhaktibuana <bhaktibuana19@gmail.com>
2022-03-18 14:22:16 +0700

    add login and register routes

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,14 @@ import AboutAdmin from "./pages/aboutAdmin";
 import Account from "./pages/account";
 import Catalog from "./pages/catalog";
 import Dashboard from "./pages/dashboard";
+import Login from "./pages/auth/login";
+import Register from "./pages/auth/register";
 import { apiUrl } from "./api/apiUrl";
 
 import "./App.scss";
 
 const RequiredAuth = () => {
-  const isAuth = true;
+  const isAuth = localStorage.getItem("access_token");
 
   if (!isAuth) {
     return <Navigate to="/" />;
@@ -24,6 +26,16 @@ const RequiredAuth = () => {
   // outlet is children of protected route
   return <Outlet />;
 };
+
+const RequireNoAuth = () => {
+  const isAuth = localStorage.getItem("access_token");
+
+  if (isAuth) {
+    return <Navigate to="/" />;
+  }
+
+  return <Outlet />;
+}
 
 function App() {
   const [isExpand, setIsExpand] = useState(true);
@@ -43,11 +55,17 @@ function App() {
           {/* public route */}
           <Route path="/" element={<Catalog />} />
 
+          {/* auth route */}
+          <Route element={<RequireNoAuth />}>
+            <Route path="/login" element={<Login apiUrl={apiUrl} />} />
+            <Route path="/register" element={<Register apiUrl={apiUrl} />} />
+          </Route>
+
           {/* protected route */}
           <Route element={<RequiredAuth />}>
             <Route path="/dashboard" element={<Dashboard appState={appState} apiUrl={apiUrl} />} />
-            <Route path="/account" element={<Account appState={appState} />} />
-            <Route path="/about-adm" element={<AboutAdmin appState={appState} />} />
+            <Route path="/account" element={<Account appState={appState} apiUrl={apiUrl} />} />
+            <Route path="/about-adm" element={<AboutAdmin appState={appState} apiUrl={apiUrl} />} />
           </Route>
         </Routes>
       </BrowserRouter>
