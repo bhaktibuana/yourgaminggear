b89815b76cba2d7706b1d7424c257624679aeedc
bhaktibuana <bhaktibuana19@gmail.com>
2022-03-18 14:14:23 +0700

    create footer for auth page

---
+++ src/components/auth/footer/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "./style.scss";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="auth-footer">
+        <p>
+          Copyright © 2022 Your Gaming Gear, Developed by{" "}
+          <a
+            href="https://github.com/bhaktibuana"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Bhakti Buana
+          </a>
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
